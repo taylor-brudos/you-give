a42973db4c3cf64e0c96b0d217fff10e871177ca
Tenju Paul <tenju@Tenjus-MacBook-Air.local>
2018-07-24 17:56:45 -0700

    more stuffs added

--- apps/first_app/urls.py
+++ apps/first_app/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     url(r'^$', views.index),
     url(r'^user/(?P<id>\d+)',views.userProfile),
     url(r'^explore$', views.explore),
-    url(r'^charity$', views.displayCharity)
+    url(r'^charity$', views.displayCharity),
+    url(r'^addgroup$', views.addGroup),
+    url(r'^checkout$', views.checkout),
+    url(r'^register$', views.register)
 ]
